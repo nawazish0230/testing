1b1111ebceb5b700036874445f78c6b1487a275d
nawazish0230 <nawazish0230@gmail.com>
2024-08-22 23:27:19 +0530

    feat: added wrapper for provider globally

--- codevolution/src/App.tsx
+++ codevolution/src/App.tsx
@@ -4,16 +4,22 @@ import "./App.css";
 import { Application } from "./components/application/application";
 import { Skills } from "./components/skills/skills";
 import { Counter } from "./components/counter/counter";
+import { MuiMode } from "./components/mui/mui-mode";
+import { AppProviders } from "./providers/app-providers";
 
 function App() {
   return (
-    <div className="App">
-      <Application />
-      <hr />
-      <Skills skills={["HTML", "CSS", "Javascript"]} />
-      <hr />
-      <Counter />
-    </div>
+    <AppProviders>
+      <div className="App">
+        <Application />
+        <hr />
+        <Skills skills={["HTML", "CSS", "Javascript"]} />
+        <hr />
+        <Counter />
+        <hr />
+        <MuiMode />
+      </div>
+    </AppProviders>
   );
 }
 
